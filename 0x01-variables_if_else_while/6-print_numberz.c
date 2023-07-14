3cc9d2a77267e051cd9f39e35a8e911ff42f6a4c
ProgramKingAbel <abemorara254@gmail.com>
2023-07-14 15:21:32 -0700

    ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 48;
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
