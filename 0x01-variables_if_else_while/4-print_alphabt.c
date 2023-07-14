eb56099f1d8786e030a24600ecc6a49a56b3e838
ProgramKingAbel <abemorara254@gmail.com>
2023-07-14 14:57:43 -0700

    alphabetic

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		if (i !== 'q' && i !== 'e')
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	return (0);
+}
