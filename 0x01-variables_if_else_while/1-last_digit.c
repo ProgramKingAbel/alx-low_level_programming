1936e86efbe9060e992b3224159dd189912b9dad
ProgramKingAbel <abemorara254@gmail.com>
2023-07-14 14:16:06 -0700

    fix linters

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int y = n % 10;
+	int y;
+	y = n % 10;
 	if (y > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
 	}
 	else if (y < 6 && y != 0)
 	{
-		printf("Last digit of %d is %d
-			       and is less than 6 and not 0\n", n, y);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	}
 	else
 	{
