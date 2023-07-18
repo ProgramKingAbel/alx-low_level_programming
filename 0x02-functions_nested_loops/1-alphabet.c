e0af7158b7e0646cd531c0bd3b90703777b35b84
ProgramKingAbel <abemorara254@gmail.com>
2023-07-18 13:25:21 -0700

    print the alphabe

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - create small letters in alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	x='a';
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
