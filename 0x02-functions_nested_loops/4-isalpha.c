0559ba622e069529f6275d27e9eff426f9cc0871
ProgramKingAbel <abemorara254@gmail.com>
2023-07-18 14:26:42 -0700

    Betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - Entry point checks for alphabet
+ * @c: character to be checked
  * Return: 1 if letter or 0 otherwise
- * c - integer to be checked
  */
 
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90)|| (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
