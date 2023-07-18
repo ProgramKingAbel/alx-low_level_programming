1f3041f8ef9564a602ec8face9c6ae24a5fe9b87
ProgramKingAbel <abemorara254@gmail.com>
2023-07-18 15:43:56 -0700

    Fix linters

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,20 @@
 
 /**
  * jack_bauer - Entry point to print minutes of the day
- * Return: void 
+ * Return: void
  */
 
 void jack_bauer(void)
 {
 	int a, x, y, z;
 
-	for (a =0; a <= 2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (x = 0; x <= 9; x++)
 		{
 			if ((a <= 1 && x <= 9) || (a <= 2 && x <= 3))
 			{
-				for (y = 0; y <=5; y++)
+				for (y = 0; y <= 5; y++)
 				{
 					for (z = 0; z <= 9; z++)
 					{
