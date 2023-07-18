1444ee90a89286ff78139a525801d08de2675d33
ProgramKingAbel <abemorara254@gmail.com>
2023-07-18 15:41:34 -0700

    Fix runtime error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,17 +13,17 @@ void jack_bauer(void)
 	{
 		for (x = 0; x <= 9; x++)
 		{
-			if ((a <= 1 && x <= 9) || (a <= 2 && b <= 3))
+			if ((a <= 1 && x <= 9) || (a <= 2 && x <= 3))
 			{
 				for (y = 0; y <=5; y++)
 				{
 					for (z = 0; z <= 9; z++)
 					{
 						_putchar(a + '0');
-						_putchar(b + '0');
+						_putchar(x + '0');
 						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
+						_putchar(y + '0');
+						_putchar(z + '0');
 						_putchar('\n');
 					}
 				}
