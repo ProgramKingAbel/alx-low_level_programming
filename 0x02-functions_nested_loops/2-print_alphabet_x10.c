4e341c102a15e7cb5337bfe083ccf9dd42d55a4b
ProgramKingAbel <abemorara254@gmail.com>
2023-07-18 13:37:07 -0700

    recursive

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+
+	x = 10;
+	while (x < 10)
+	{
+		char y;
+		y = 'a';
+		while (y <= 'z')
+		{
+			_putchar(y);
+			y++;
+		}
+		x++;
+	}
+	_print('\n');
+}
