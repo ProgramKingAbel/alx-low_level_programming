64fd591fdf12cdc6ad4d86f20fe4f83ee929e7b3
ProgramKingAbel <abemorara254@gmail.com>
2023-07-20 12:51:28 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point check for digit
+ * @c: value to be checked
+ * Return: 1 if truthy 0 if falsy
+ */
+
+int _isidigt(int c)
+{
+	if (c >= 48 && c < 57)
+	{
+		return (1);
+	}
+	return (0);
+}
