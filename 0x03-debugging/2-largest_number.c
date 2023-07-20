74ea29d9c001686dd1d4b52bc9dd0f4899532926
ProgramKingAbel <abemorara254@gmail.com>
2023-07-20 09:16:35 -0700

    Largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > b)
+	else if (b < a && a < c)
 	{
 		largest = c;
 	}
-	else
+	else if (c <  b)
 	{
 		largest = b;
+	}
+	else
+	{
+		largest = c;
 	}
 	return (largest);
 }
