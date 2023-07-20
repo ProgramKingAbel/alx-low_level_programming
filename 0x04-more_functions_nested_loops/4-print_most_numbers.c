6694e1068179769fda27db8d860bef2430fefbee
ProgramKingAbel <abemorara254@gmail.com>
2023-07-20 13:47:18 -0700

    fix linters

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,19 @@
 
 /**
  * print_most_numbers - Prints nums to stdout
- * Return: void
+ * Description: Print nums except 2 and 4
+ * Return: nums 0 - 9
  */
 
 void print_most_numbers(void)
 {
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i == 2 || i == 4)
 		{
-			_putchar(i + '0');
+			continue;
 		}
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
