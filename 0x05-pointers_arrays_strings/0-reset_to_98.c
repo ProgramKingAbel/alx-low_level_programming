838186f36cc877bab12d3419d5993411ad5bc267
ProgramKingAbel <abemorara254@gmail.com>
2023-07-24 13:54:32 -0700

    reset int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point dereference pointer to int and update
+ * point to 98
+ * @n: input pointer
+ * Return: empty
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
