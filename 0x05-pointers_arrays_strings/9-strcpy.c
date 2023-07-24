29ad8853c969a93bf3fbe241318954e3549cd0ea
ProgramKingAbel <abemorara254@gmail.com>
2023-07-24 15:42:45 -0700

    dereferencing

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - cop string to buffer
+ * @dest: to be copied to
+ * @src: value to be copied
+ * Return: pointer char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (i = 0; src[x] != '\0'; x++)
+		dest[x] = src[x];
+	dest[x++] = '\0';
+	return (dest);
+}
