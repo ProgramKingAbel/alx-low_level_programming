347f7fd1829fb302d228d834d68cb1c4c93ac9dc
ProgramKingAbel <abemorara254@gmail.com>
2023-07-24 14:09:39 -0700

    Get string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Counts length of string
+ * @s: string to be count
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+	return (0);
+}
