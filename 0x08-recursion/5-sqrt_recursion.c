3bbf3dbc0e8c45981f3b2c4d401f13556d47720c
ProgramKingAbel <abemorara254@gmail.com>
2023-08-03 06:05:45 -0700

    Fix lintrs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
-/**
- * _sqrt_recursion - Get square root of num
- * @n: num to get square root of
- * Return: sqrt of the num
- */
+int actual_sqrt_recursion(int n, int i);
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
