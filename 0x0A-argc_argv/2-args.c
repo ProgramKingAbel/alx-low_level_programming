ee5db84196f2f84ca7e35de1c70270d232f56d4f
ProgramKingAbel <abemorara254@gmail.com>
2023-08-07 14:37:22 -0700

    Print all argv[i]

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,18 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - App entry point
+ * @argc: num of ars passed
+ * @argv: array of actual args passed
+ * Return: Always (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+		printf("%s\n", argv[a]);
+	return (0);
+}
