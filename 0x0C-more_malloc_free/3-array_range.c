0b3d8b9dd5c43db2c65be04fcd1229575265f5be
ProgramKingAbel <abemorara254@gmail.com>
2023-08-10 15:12:26 -0700

    Create arr of int

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Entry point - creates array of integers
+ * @min: start point
+ * @max: end point
+ * Return: pointer to newly created arr
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int a, len;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+
+	p = malloc(sizeof(int) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (a = 0; min <= max; a++)
+		p[a] = min++;
+
+	return (p);
+}
+
