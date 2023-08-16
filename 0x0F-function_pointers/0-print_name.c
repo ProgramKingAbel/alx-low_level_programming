dcb21d160558e8ee3cc5fb675bd81bf13328f53f
ProgramKingAbel <abemorara254@gmail.com>
2023-08-16 07:25:21 -0700

    Implement function pointer to puts name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: name to be put
+ * @f: pointer to func
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
