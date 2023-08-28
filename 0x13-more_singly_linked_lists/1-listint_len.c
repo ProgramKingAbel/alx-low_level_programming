506161f31c26ab3474c0599afc78885814763abc
ProgramKingAbel <abemorara254@gmail.com>
2023-08-28 08:32:30 -0700

    COunt nodes num in linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in linked list
+ * @h: linked list
+ * Return: node total
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
