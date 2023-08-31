8dfdc544592c4e4c8e62b2337320d53cc2995194
ProgramKingAbel <abemorara254@gmail.com>
2023-08-31 03:57:15 -0700

    FIx linters

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int a;
-
-	unsigned int val = 0;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (a = 0; b[0]; a++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[a] < '0' || b[a] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		val = 2 * val + (b[a] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (val);
+
+	return (dec_val);
 }
